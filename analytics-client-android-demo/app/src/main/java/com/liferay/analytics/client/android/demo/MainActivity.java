e96c0520dcc3f72a22bc2ae307ebf113a86d6f76
Igor Matos <igor.matos@liferay.com>
2018-02-26 11:02:01 -0300

    LPS-78114 Source formatting

--- analytics-client-android-demo/app/src/main/java/com/liferay/analytics/client/android/demo/MainActivity.java
+++ analytics-client-android-demo/app/src/main/java/com/liferay/analytics/client/android/demo/MainActivity.java
@@ -1,13 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.analytics.client.android.demo;
 
 import android.os.Bundle;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import android.text.Editable;
+
 import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,13 +37,80 @@ import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
+/**
+ * @author Igor Matos
+ */
 public class MainActivity extends AppCompatActivity {
+
+	public void getUserId() {
+		TextView userIdTV = findViewById(R.id.userId);
+
+		Flowable flowable = Flowable.fromCallable(
+			() -> {
+				String userId = LiferayAnalytics.getUserId(
+					"igor.matos@liferay.com", "igor", "liferay.com");
+
+				Log.d("ANALYTICS-DEMO", userId);
+
+				return userId;
+			});
+
+		flowable = flowable.subscribeOn(Schedulers.newThread());
+
+		flowable = flowable.observeOn(AndroidSchedulers.mainThread());
+
+		flowable.subscribe(userId -> userIdTV.setText((String)userId));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void sendAnalytics(String eventId, String userId) {
+		Flowable flowable = Flowable.fromCallable(
+			() -> {
+				LiferayAnalytics.sendAnalytics("liferay.com", userId, eventId);
+
+				return Observable.empty();
+			});
+
+		flowable = flowable.subscribeOn(Schedulers.newThread());
+
+		flowable = flowable.observeOn(AndroidSchedulers.mainThread());
+
+		flowable.subscribe();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = findViewById(R.id.toolbar);
+
 		setSupportActionBar(toolbar);
 
 		EditText editText = findViewById(R.id.eventId);
@@ -33,69 +119,15 @@ public class MainActivity extends AppCompatActivity {
 		getUserId();
 
 		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				sendAnalytics(editText.getText().toString(), userIdTV.getText().toString());
-			}
-		});
 
+		fab.setOnClickListener(
+			view -> {
+				Editable eventIdEditable = editText.getText();
+				CharSequence userIdCharSequence = userIdTV.getText();
+
+				sendAnalytics(
+					eventIdEditable.toString(), userIdCharSequence.toString());
+			});
 	}
 
-	public void getUserId() {
-		TextView userIdTV = findViewById(R.id.userId);
-
-		Flowable.fromCallable(() -> {
-			String userId = LiferayAnalytics.getUserId("igor.matos@liferay.com", "igor", "liferay.com");
-			Log.d("CAMPEAO", userId);
-			return userId;
-		}).subscribeOn(Schedulers.newThread())
-			.observeOn(AndroidSchedulers.mainThread())
-			.subscribe(userId -> {
-				userIdTV.setText(userId);
-			}, Throwable::printStackTrace);
-	}
-
-	public void sendAnalytics(String eventId, String userId) {
-		Flowable.fromCallable(() -> {
-			LiferayAnalytics.sendAnalytics("liferay.com", userId, eventId);
-			return Observable.empty();
-		}).subscribeOn(Schedulers.newThread())
-			.observeOn(AndroidSchedulers.mainThread())
-			.subscribe(System.out::println, Throwable::printStackTrace);
-	}
-
-//	public void sendAnalytics(String eventId) {
-//		Flowable.fromCallable(() -> {
-//			String userId = LiferayAnalytics.getUserId("igor.matos@liferay.com", "igor", "liferay.com");
-//			Log.d("CAMPEAO", userId);
-//			return userId;
-//		}).doOnNext(userId -> {
-//			LiferayAnalytics.sendAnalytics("liferay.com", userId, eventId);
-//		}).subscribeOn(Schedulers.newThread())
-//			.observeOn(AndroidSchedulers.mainThread())
-//			.subscribe(System.out::println, Throwable::printStackTrace);
-//	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 }
--- analytics-client-android-demo/app/src/test/java/com/liferay/analytics/client/android/demo/ExampleUnitTest.java
+++ analytics-client-android-demo/app/src/test/java/com/liferay/analytics/client/android/demo/ExampleUnitTest.java
@@ -1,17 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.analytics.client.android.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @author Igor Matos
  */
 public class ExampleUnitTest {
+
 	@Test
-	public void addition_isCorrect() throws Exception {
-		assertEquals(4, 2 + 2);
+	public void testDefault() throws Exception {
+		Assert.assertEquals(4, 2 + 2);
 	}
+
 }
